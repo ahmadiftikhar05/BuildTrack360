64143fdc06c213f4c2ae144078f4997458e17659
Muhammad Omer Jawaid <omerjawaid0@gmail.com>
2024-12-16 23:13:48 +0500

    Update Main.java

--- src/main/java/com/example/buildtrack360/Main.java
+++ src/main/java/com/example/buildtrack360/Main.java
@@ -12,10 +12,11 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         try {
             // Load the FXML file from the resources folder
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/buildpath360/Login.fxml"));
-            Parent root = loader.load();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/buildtrack360/Login.fxml"));
+            AnchorPane root = loader.load();
+            Scene scene=new Scene(root)
             primaryStage.setTitle("Login");
-            primaryStage.setScene(new Scene(root));
+            primaryStage.setScene(scene);
             primaryStage.show();
         } catch (Exception e) {
             e.printStackTrace();
