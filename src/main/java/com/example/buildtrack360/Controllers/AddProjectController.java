954f8a634f53bb93f8b212400888f1f7cca6ef4b
Ahmad Iftikhar Khan <01-131232-009@student.bahria.edu.pk>
2024-12-29 01:00:34 +0500

    table view

--- src/main/java/com/example/buildtrack360/Controllers/AddProjectController.java
+++ src/main/java/com/example/buildtrack360/Controllers/AddProjectController.java
@@ -3,6 +3,7 @@ package com.example.buildtrack360.Controllers;
 import com.example.buildtrack360.Database.DatabaseConnection;
 import com.example.buildtrack360.Database.LoadDatabase;
 import com.example.buildtrack360.Backend.Project.Project;
+import com.example.buildtrack360.project;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -12,39 +13,39 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.control.TextField;
 import java.io.File;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class AddProjectController {
     @FXML
     private TextField filePathField;
     @FXML
     private TextField NameTextField;
-     @FXML
+    @FXML
     private TextField AmountTextField;
-     @FXML
+    @FXML
     private ComboBox<String> CustomerCombobox;
-     @FXML
-     private Label WarningLabel;
-     @FXML
-     private ComboBox<String> StagesCombobox;
+    @FXML
+    private Label WarningLabel;
+    @FXML
+    private ComboBox<String> StagesCombobox;
+
     @FXML
     public void initialize() {
         try {
             populateCustomerCombobox();
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Something is Wrong");
-        }       System.out.println("Initialization complete"); // Debug statement
+        }
+        System.out.println("Initialization complete"); // Debug statement
     }
 
-    void populateCustomerCombobox(){
+    void populateCustomerCombobox() {
         DatabaseConnection con = new DatabaseConnection();
         ObservableList<String> CustomerList = FXCollections.observableArrayList();
 
@@ -58,13 +59,12 @@ public class AddProjectController {
                 System.out.println(customerName);
             }
 
-           CustomerCombobox.setItems(CustomerList);
+            CustomerCombobox.setItems(CustomerList);
 
         } catch (SQLException e) {
             showAlert("Error", "Failed to load roles: " + e.getMessage());
             e.printStackTrace();
-        }
-        catch(Error e){
+        } catch (Error e) {
             System.out.println("Something went wrong");
         }
     }
@@ -93,16 +93,15 @@ public class AddProjectController {
 
     public void handleAddProjectButton(ActionEvent actionEvent) {
 
-        if (!NameTextField.getText().isEmpty()||AmountTextField.getText().isEmpty()||CustomerCombobox.getValue()!=null||filePathField.getText().isEmpty()) {
-            int Amount=0,StageID=1;
-            DatabaseConnection con= new DatabaseConnection();
+        if (!NameTextField.getText().isEmpty() || AmountTextField.getText().isEmpty() || CustomerCombobox.getValue() != null || filePathField.getText().isEmpty()) {
+            int Amount = 0, StageID = 1;
+            DatabaseConnection con = new DatabaseConnection();
 
 
-            try(Connection connection=con.GetConnection();
-                PreparedStatement preparedStatement=connection.prepareStatement("SELECT ID FROM customers WHERE Name = ?");)
-            {
-                preparedStatement.setString(1,"New");
-                ResultSet resultSet=preparedStatement.executeQuery();
+            try (Connection connection = con.GetConnection();
+                 PreparedStatement preparedStatement = connection.prepareStatement("SELECT ID FROM customers WHERE Name = ?");) {
+                preparedStatement.setString(1, "New");
+                ResultSet resultSet = preparedStatement.executeQuery();
 
                 if (resultSet.next()) {
                     StageID = resultSet.getInt("ID");
@@ -114,28 +113,26 @@ public class AddProjectController {
             }
             try {
                 Amount = Integer.parseInt(AmountTextField.getText());
-                LoadDatabase Load=new LoadDatabase();
+                LoadDatabase Load = new LoadDatabase();
                 Load.LoadCustomers();
-                Load.CustomersList.tempNode=Load.CustomersList.GetHead();
+                Load.CustomersList.tempNode = Load.CustomersList.GetHead();
 
-                while(Load.CustomersList.tempNode != null) {
+                while (Load.CustomersList.tempNode != null) {
                     if (Load.CustomersList.tempNode.data.getName().equals(CustomerCombobox.getValue())) {
-                        System.out.println("From DB: "+Load.CustomersList.tempNode.data.getName());
+                        System.out.println("From DB: " + Load.CustomersList.tempNode.data.getName());
                         break;
-                    }
-                    else{
-                        Load.CustomersList.tempNode=Load.CustomersList.tempNode.next;
+                    } else {
+                        Load.CustomersList.tempNode = Load.CustomersList.tempNode.next;
                     }
                 }
 
-                int CustomerID= Load.CustomersList.tempNode != null ? Load.CustomersList.tempNode.data.getID() : 1;
+                int CustomerID = Load.CustomersList.tempNode != null ? Load.CustomersList.tempNode.data.getID() : 1;
 
-               Project project = new Project(NameTextField.getText(),CustomerID,Amount,filePathField.getText(),StageID);//Add Stages
-               project.AddProject();
+                Project project = new Project(NameTextField.getText(), CustomerID, Amount, filePathField.getText(), StageID);//Add Stages
+                project.AddProject();
 
-                showAlert("Success","Added a New Project");
-            }
-            catch(NumberFormatException e){
+                showAlert("Success", "Added a New Project");
+            } catch (NumberFormatException e) {
                 System.out.println("Incorrect Amount");
                 WarningLabel.setText("Incorrect Amount");
                 WarningLabel.setVisible(true);
@@ -143,30 +140,28 @@ public class AddProjectController {
 
 
         } else {
-            if(NameTextField.getText().isEmpty()&&filePathField.getText().isEmpty()&&AmountTextField.getText().isEmpty()&&CustomerCombobox.getValue()==null){
+            if (NameTextField.getText().isEmpty() && filePathField.getText().isEmpty() && AmountTextField.getText().isEmpty() && CustomerCombobox.getValue() == null) {
                 WarningLabel.setText("All Fields are Empty");
                 WarningLabel.setVisible(true);
-            }
-            else if(NameTextField.getText().isEmpty()){
+            } else if (NameTextField.getText().isEmpty()) {
                 WarningLabel.setText("Name is Empty");
                 WarningLabel.setVisible(true);
-            }
-            else if(AmountTextField.getText().isEmpty()){
+            } else if (AmountTextField.getText().isEmpty()) {
                 WarningLabel.setText("Amount is Empty");
                 WarningLabel.setVisible(true);
             } else if (filePathField.getText().isEmpty()) {
                 WarningLabel.setText("File Path is Empty");
                 WarningLabel.setVisible(true);
-            } else if (CustomerCombobox.getValue()==null) {
+            } else if (CustomerCombobox.getValue() == null) {
                 WarningLabel.setText("Customer not chosen");
                 WarningLabel.setVisible(true);
-            }
-            else{
+            } else {
                 WarningLabel.setText("Some Fields are Empty");
                 WarningLabel.setVisible(true);
             }
         }
     }
+
     private void showAlert(String title, String message) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle(title);
@@ -174,15 +169,84 @@ public class AddProjectController {
         alert.setContentText(message);
         alert.showAndWait();
     }
+
+
+    public void pathButtonOnClick(ActionEvent actionEvent) {
+        try {
+            Stage stage = new Stage();
+            Stage currentStage = (Stage) NameTextField.getScene().getWindow();
+
+            // Load the FXML file from the resources folder
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/buildtrack360/BuildPath.fxml"));
+            AnchorPane root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setTitle("BuildTrack360");
+            stage.setScene(scene);
+            stage.setMaximized(true);
+            stage.setResizable(false);
+
+
+            stage.show();
+            currentStage.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public void dashboardButtonOnClick(ActionEvent actionEvent) {
         try {
-            Stage stage=new Stage();
-            Stage currentStage = (Stage) AmountTextField.getScene().getWindow();
+            Stage stage = new Stage();
+            Stage currentStage = (Stage) NameTextField.getScene().getWindow();
 
             // Load the FXML file from the resources folder
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/buildtrack360/Dashboard.fxml"));
             AnchorPane root = loader.load();
-            Scene scene=new Scene(root);
+            Scene scene = new Scene(root);
+            stage.setTitle("BuildTrack360");
+            stage.setScene(scene);
+            stage.setMaximized(true);
+            stage.setResizable(false);
+
+
+            stage.show();
+            currentStage.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void projectsButtonOnClick(ActionEvent actionEvent) {
+        try {
+            Stage stage = new Stage();
+            Stage currentStage = (Stage) NameTextField.getScene().getWindow();
+
+            // Load the FXML file from the resources folder
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/buildtrack360/Projects.fxml"));
+            AnchorPane root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setTitle("BuildTrack360");
+            stage.setScene(scene);
+            stage.setMaximized(true);
+            stage.setResizable(false);
+
+
+            stage.show();
+            currentStage.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void signupButtonOnClick(ActionEvent actionEvent) {
+        try {
+            Stage stage = new Stage();
+            Stage currentStage = (Stage) NameTextField.getScene().getWindow();
+
+            // Load the FXML file from the resources folder
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/buildtrack360/Signup.fxml"));
+            AnchorPane root = loader.load();
+            Scene scene = new Scene(root);
             stage.setTitle("BuildTrack360");
             stage.setScene(scene);
             stage.setMaximized(true);
--- src/main/java/com/example/buildtrack360/Controllers/DasboardController.java
+++ src/main/java/com/example/buildtrack360/Controllers/DasboardController.java
@@ -1,22 +1,48 @@
 package com.example.buildtrack360.Controllers;
 
+import com.example.buildtrack360.project;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Alert;
+import javafx.scene.control.*;
 
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+
+import com.example.buildtrack360.Database.DatabaseConnection;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class DasboardController {
 
 @FXML
 Label DashboadTitle;
+    @FXML
+    private TableView<project> projectsummaryTable;
+
+    @FXML
+    private TableColumn<project, String> nameColumn;
+
+    @FXML
+    private TableColumn<project, String> projectmanagerColumn;
+
+    @FXML
+    private TableColumn<project, String> duedateColumn;
+
+    @FXML
+    private TableColumn<project, String> statusColumn;
+
     public void pathButtonOnClick(ActionEvent actionEvent)
     {
         try {
@@ -106,4 +132,39 @@ Label DashboadTitle;
             e.printStackTrace();
         }
     }
+
+    private ObservableList<project> projectList = FXCollections.observableArrayList();
+
+    @FXML
+    public void initialize() {
+        loadProjectData();
+    }
+
+    private void loadProjectData() {
+        try {
+            Connection connection = DatabaseConnection.GetConnection();
+            String query = "SELECT Name, ProjectManager, Agreement, CompletePercent FROM projects";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                String name = resultSet.getString("Name");
+                String projectManager = resultSet.getString("ProjectManager");
+                String status = resultSet.getString("CompletePercent") + "%";
+
+                projectList.add(new project(name, projectManager,status));
+            }
+
+            nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+            projectmanagerColumn.setCellValueFactory(new PropertyValueFactory<>("projectManager"));
+            statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+
+            projectsummaryTable.setItems(projectList);
+
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
